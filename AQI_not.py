afd8d214150eebfdbe7399916f61b9bee3c58f21
WhonderWy <yinyinyinyinyinyinyin@hotmail.com>
2019-12-12 19:08:47 +1100

    Added scale, begun building the parsing function
    Signed-off-by: WhonderWy <yinyinyinyinyinyinyin@hotmail.com>

--- AQI_not.py
+++ AQI_not.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Original Author: William
 # Date created: 2019/12/12
@@ -6,8 +6,51 @@
 # What this does:
 # A simple script that outputs necessary data to terminal or elsewhere based on config.
 
+from bs4 import BeautifulSoup
+
+CONFIG = "config.json"
+SETTINGS = {}
+SCALE = {
+    "VERY GOOD": 33,
+    "GOOD": 66,
+    "FAIR": 99,
+    "POOR": 149,
+    "VERY POOR": 199,
+    "HAZARDOUS": 200,
+
+    "scale": 33
+}
+
+# class Settings():
+# 	def __init__(self):
+# 		pass
+
+def read_config():
+    import json
+    global SETTINGS
+
+    with open(CONFIG, "w+") as config:
+        SETTINGS = json.load(config)
+
+def print_config():
+    for field in SETTINGS:
+        print(field)
+
+def get_html():
+    import requests
+    html_data = requests.get(str(SETTINGS["site"]))
+    return html_data.text
+
+def get_value():
+    soup = BeautifulSoup(markup=get_html(), features="lxml")
+    return result
+
+def print_data():
+    for field in SETTINGS["interested_fields"]:
+        value = get_value()
+        print(f"{field}: {value}")
 
 
 
 if __name__ == "__main__":
-	pass
+    pass
