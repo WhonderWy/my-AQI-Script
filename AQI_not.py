fef26e993069fe57b043926ce08ce89d1607b4df
WhonderWy <yinyinyinyinyinyinyin@hotmail.com>
2019-12-12 18:30:24 +1100

    Shebang and comments(?) in.
    Signed-off-by: WhonderWy <yinyinyinyinyinyinyin@hotmail.com>

---
+++ AQI_not.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Original Author: William
+# Date created: 2019/12/12
+# Reason: The air quality settled down around my area and I wasn't worrying about breathing for once so I considered writing a simple script.
+# What this does:
+# A simple script that outputs necessary data to terminal or elsewhere based on config.
+
+
+
