d66dca2157b33a3806c56f5d584fb1bdff451256
WhonderWy <yinyinyinyinyinyinyin@hotmail.com>
2019-12-20 15:16:57 +1100

    Finally getting to understanding this beautifulsoup
    Signed-off-by: WhonderWy <yinyinyinyinyinyinyin@hotmail.com>

--- AQI_not.py
+++ AQI_not.py
@@ -2,7 +2,7 @@
 
 # Original Author: William
 # Date created: 2019/12/12
-# Reason: The air quality settled down around my area and I wasn't worrying about breathing for once so I considered writing a simple script.
+# Reason: The air quality settled down around my area and I wasn"t worrying about breathing for once so I considered writing a simple script.
 # What this does:
 # A simple script that outputs necessary data to terminal or elsewhere based on config.
 
@@ -18,7 +18,6 @@ SCALE = {
     "POOR": 149,
     "VERY POOR": 199,
     "HAZARDOUS": 200,
-
     "scale": 33,
 }
 
@@ -39,36 +38,51 @@ fields = {
 # 	def __init__(self):
 # 		pass
 
+
 def read_config():
     import json
+
     global settings
 
     with open(CONFIG, "r+") as config:
         settings = json.load(config)
 
+
 def print_config():
     for field in settings:
         print(field)
+
 
 def get_html():
     import requests
 
     html_data = requests.get(str(settings["site"]))
-    
+
     return html_data.text
+
 
 def get_values():
     global fields
 
     soup = BeautifulSoup(markup=get_html(), features="lxml")
-    table = soup.find_all('table')[5]
+    table = soup.find_all("table", class_="aqi")[0]
+    # site = table.find_all(id="site")
+    targets = table.find_all("tr")
 
-    for row in table.find_all('tr'):
-        # Incidentally, it's #43
-        if (row.find_all(settings["location"])):
-            columns = row.find_all('td')
-            for column, key in zip(columns, fields):
-                fields[key] = column.get_text()
+    for row in targets:
+        # Incidentally, it"s #43
+        # class_="Site"
+        if row.text.find(settings["location"]):
+            text = row.text.splitlines()
+            for line, key in text, fields:
+                if settings["location"] != line:
+                    fields[key] = line
+        # current = row.find_all("td", settings["location"])
+        # if current:
+        #     columns = row.find_all("td")
+        #     for column, key in zip(columns, fields):
+        #         fields[key] = column.get_text()
+
 
 def print_data():
     scale = None
@@ -81,6 +95,17 @@ def print_data():
         string = f"{field:<20} is {scale:^10} at {value:>5}"
         print(string)
 
+
+def send_notification():
+    import platform
+
+    osName = platform.system()
+    if osName == "Linux":
+        pass
+    elif osName == "Windows":
+        pass
+    else:
+        pass
 
 
 if __name__ == "__main__":
