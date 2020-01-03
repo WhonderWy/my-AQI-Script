8a259e50437f534fcbf66b71104df11b74505f8c
WhonderWy <yinyinyinyinyinyinyin@hotmail.com>
2020-01-03 22:05:55 +1100

    Added an install.sh
    Signed-off-by: WhonderWy <yinyinyinyinyinyinyin@hotmail.com>

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Ensuring python3 is installed..."
+sudo apt update && sudo apt install python3
+
+echo "Installing pip dependencies..."
+pip3 install --user BeautifulSoup4 lxml
+
+echo "Cloning repository..."
+git clone https://github.com/WhonderWy/my-AQI-Script.git
+
+echo "Creating link..."
+ln my-AQI-Script/AQI_not.py ~/.local/bin/AQI
+
+echo "Complete. Restart or source relevant bashrc or zshrc in order to run AQI from anywhere."
+
+echo "In the meantime, I'll run it and see if it works."
+my-AQI-Script/AQI_not.py
